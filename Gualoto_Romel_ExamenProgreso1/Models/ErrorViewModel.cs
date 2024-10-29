4dba3a18ce49520f5346cab6688d2db5327f7a4b
rggua <rggua@ROMELLAPTOP>
2024-10-29 07:18:41 -0500

    Agregar archivos de proyecto.

---
+++ Gualoto_Romel_ExamenProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gualoto_Romel_ExamenProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
