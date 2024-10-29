c6392de928df8ccecf63f8981184221c47337ac8
rggua <rggua@ROMELLAPTOP>
2024-10-29 09:00:06 -0500

    ultimo commit

---
+++ Gualoto_Romel_ExamenProgreso1/Controllers/CelularsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gualoto_Romel_ExamenProgreso1.Models;
+
+namespace Gualoto_Romel_ExamenProgreso1.Controllers
+{
+    public class CelularsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelularsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Celulars
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Celulares.ToListAsync());
+        }
+
+        // GET: Celulars/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var celular = await _context.Celulares
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (celular == null)
+            {
+                return NotFound();
+            }
+
+            return View(celular);
+        }
+
+        // GET: Celulars/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Celulars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Modelo,Año,Precio")] Celular celular)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(celular);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(celular);
+        }
+
+        // GET: Celulars/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var celular = await _context.Celulares.FindAsync(id);
+            if (celular == null)
+            {
+                return NotFound();
+            }
+            return View(celular);
+        }
+
+        // POST: Celulars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Modelo,Año,Precio")] Celular celular)
+        {
+            if (id != celular.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(celular);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CelularExists(celular.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(celular);
+        }
+
+        // GET: Celulars/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var celular = await _context.Celulares
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (celular == null)
+            {
+                return NotFound();
+            }
+
+            return View(celular);
+        }
+
+        // POST: Celulars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var celular = await _context.Celulares.FindAsync(id);
+            if (celular != null)
+            {
+                _context.Celulares.Remove(celular);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CelularExists(int id)
+        {
+            return _context.Celulares.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Gualoto_Romel_ExamenProgreso1/Controllers/RGualotoesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gualoto_Romel_ExamenProgreso1.Models;
+
+namespace Gualoto_Romel_ExamenProgreso1.Controllers
+{
+    public class RGualotoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RGualotoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RGualotoes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Apellidos.Include(r => r.Celular);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: RGualotoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rGualoto = await _context.Apellidos
+                .Include(r => r.Celular)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rGualoto == null)
+            {
+                return NotFound();
+            }
+
+            return View(rGualoto);
+        }
+
+        // GET: RGualotoes/Create
+        public IActionResult Create()
+        {
+            ViewData["CelularId"] = new SelectList(_context.Celulares, "Id", "Modelo");
+            return View();
+        }
+
+        // POST: RGualotoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Edad,IngresoMensual,Activo,FechaNacimiento,CelularId")] RGualoto rGualoto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rGualoto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CelularId"] = new SelectList(_context.Celulares, "Id", "Modelo", rGualoto.CelularId);
+            return View(rGualoto);
+        }
+
+        // GET: RGualotoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rGualoto = await _context.Apellidos.FindAsync(id);
+            if (rGualoto == null)
+            {
+                return NotFound();
+            }
+            ViewData["CelularId"] = new SelectList(_context.Celulares, "Id", "Modelo", rGualoto.CelularId);
+            return View(rGualoto);
+        }
+
+        // POST: RGualotoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Edad,IngresoMensual,Activo,FechaNacimiento,CelularId")] RGualoto rGualoto)
+        {
+            if (id != rGualoto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rGualoto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RGualotoExists(rGualoto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CelularId"] = new SelectList(_context.Celulares, "Id", "Modelo", rGualoto.CelularId);
+            return View(rGualoto);
+        }
+
+        // GET: RGualotoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rGualoto = await _context.Apellidos
+                .Include(r => r.Celular)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rGualoto == null)
+            {
+                return NotFound();
+            }
+
+            return View(rGualoto);
+        }
+
+        // POST: RGualotoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rGualoto = await _context.Apellidos.FindAsync(id);
+            if (rGualoto != null)
+            {
+                _context.Apellidos.Remove(rGualoto);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RGualotoExists(int id)
+        {
+            return _context.Apellidos.Any(e => e.Id == id);
+        }
+    }
+}
